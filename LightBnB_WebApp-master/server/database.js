9e22f01cd0dfbc9fccecfe1ffcb64283ab43b799
Dexyod <dexyod43@gmail.com>
2020-08-20 13:50:56 +0000

    added query functionality ti DB queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -16,16 +16,18 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  const queryParams = [email];
+  const queryString = `SELECT * FROM users WHERE email = $1`;
+  return pool
+    .query(queryString, queryParams)
+    .then((res) => {
+      if (res.rows) {
+        return res.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => err.stack);
 };
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -35,7 +37,18 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  const queryParams = [id];
+  const queryString = `SELECT * FROM users WHERE id = $1`;
+  return pool
+    .query(queryString, queryParams)
+    .then((res) => {
+      if (res.rows) {
+        return res.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => err.stack);
 };
 exports.getUserWithId = getUserWithId;
 
@@ -45,10 +58,19 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const queryParams = [user.name, user.email, user.password];
+  const queryString = `INSERT INTO users (name, email, password)
+      VALUES ($1, $2, $3) RETURNING *;`;
+  return pool
+    .query(queryString, queryParams)
+    .then((res) => {
+      if (res.rows) {
+        return res.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => err.stack);
 };
 exports.addUser = addUser;
 
